920c3965f4a20770d1e561989a2ddbdea2f82948
AlexDyatlov <aleks78.gooogle@gmail.com>
2020-09-23 19:15:12 +0300

    homework-2

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// const firstRow = 'мама мыла раму';
+// const secondRow = 'собака друг человека';
+firstRow = prompt('Введите первую строку');
+secondRow = prompt('Введите вторую строку');
+
+function getAmountChar(char, row) {
+    let counter = 0;
+    for(let i = 0; i < row.length; i++){
+        if (row.charAt(i) === char) {
+            counter++;
+        }
+    }
+    return counter;
+};
+
+function getRow(firstRow, secondRow) {
+    return getAmountChar('а', firstRow) > getAmountChar('а', secondRow) ? firstRow : secondRow;
+};
+
+alert(getRow(firstRow, secondRow));
